a4d4073df37d2118e9a449c8c74f0b7e9d44651e
Aleksander W <us.aw@hotmail.com>
2021-09-14 08:19:58 +0200

    Event Setup messages

--- cogs/eventSetup.py
+++ cogs/eventSetup.py
@@ -44,8 +44,43 @@ class EventSetup(commands.Cog):
 
     async def NewEventMsg(self):
         self.load_data() 
+        new_event_channel = self.bot.get_channel(self.id["new_events_channel"])
 
         new_event_embed = discord.Embed(title = "Create new events here!", color=0x303136)
         new_event_embed.add_field(name = "\u200B", value = "**React to this mesage with the type of event you would like to create", inline = False)
-        new_event_embed.add_field(name = "\u200B", value = ":alarm_clock: Doodle(Time finder)\n:date: Meeting\n:game_die: Game Jam")
+        new_event_embed.add_field(name = "\u200B", value = ":alarm_clock: Doodle(Date finder)\n:date: Meeting\n:game_die: Game Jam")
+        new_event_msg = await new_event_channel.send(embed = new_event_embed)
+        new_event_msg_id = new_event_msg.id
+
+        await new_event_msg.add_reaction(emoji = '\u23F0')          # alarm clock
+        await new_event_msg.add_reaction(emoji = '\U0001F4C5')      # date
+        await new_event_msg.add_reaction(emoji = '\U0001F3B2')      # game die
+
+        self.id["new_event_msg_id"] = new_event_msg_id
+        fh.save_file(self.id, "id")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        self.load_data()
+        if payload.user_id == self.id['bot_id']:
+            return
+
+        elif payload.message_id == self.id['new_event_msg_id']:
+            guild = self.got.get_guild(payload.guild_id)
+            events_channel = self.bot.get_channel(self.id['events_channel'])
+            reaction = payload.emoji.name
+
+            event_type = "none"
+            if reaction == '\u23F0':            # alarm clock
+                event_type = "doodle"
+            elif reaction == '\U0001F4C5':
+                event_type = "meeting"
+            elif reaction == '\U0001F3B2':
+                event_type = "jam"
+            await events_channel.purge(limit = 5)
+
+            date_embed = discord.Embed(title = f"You have chosen to create a **{event_type}** event", color=0x303136)
+            
+        
+
 
