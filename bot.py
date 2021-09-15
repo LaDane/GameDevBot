a470fffca27340fb6d5dddcaf573700bed42582e
LaDane <us.aw@hotmail.com>
2021-09-15 11:17:49 +0200

    More work on event setup

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 class GameDevBot(commands.Bot):
     def __init__(self):
-        self.cogs = [p.stem for p in Path(".").glob("./cogs/*.py")]
+        self._cogs = [p.stem for p in Path(".").glob("./cogs/*.py")]
         super().__init__(
             command_prefix = self.prefix,
             case_insensitive = True,
@@ -19,7 +19,7 @@ class GameDevBot(commands.Bot):
         print("Running setup...")
         for cog in self._cogs:
             self.load_extension(f"cogs.{cog}")
-            print("fLoaded '{cog}' cog.")
+            print(f"Loaded '{cog}' cog.")
         print("Setup complete.")
 
     def run(self):
@@ -51,33 +51,33 @@ class GameDevBot(commands.Bot):
         resumeTime = datetimeNow.strftime("%A  %d-%m-%Y  %H:%M:%S")
         print(f"Bot resumed at:            {resumeTime}")
 
-    async def on_error(self, err, *args, **kwargs):
-        try:
-            raise Exception()
-        except Exception as e:
-            raise e
+    # async def on_error(self, err, *args, **kwargs):
+    #     try:
+    #         raise Exception()
+    #     except Exception as e:
+    #         raise e
+
+    # async def on_command_error(self, ctx, exc):
+    #     rdf_guild = self.get_guild(rdfID)
+    #     leifbot_channel = rdf_guild.get_channel(leifBotChannelID)        
+    #     error_exception = getattr(exc, "original", exc)
+    #     error_embed = discord.Embed(description = f"Error\n\n**{error_exception}**", color=0x303136)
+    #     await leifbot_channel.send(embed = error_embed)
+    #     raise getattr(exc, "original", exc)
 
     async def on_command_error(self, ctx, exc):
-        rdf_guild = self.get_guild(rdfID)
-        leifbot_channel = rdf_guild.get_channel(leifBotChannelID)        
-        error_exception = getattr(exc, "original", exc)
-        error_embed = discord.Embed(description = f"Error\n\n**{error_exception}**", color=0x303136)
-        await leifbot_channel.send(embed = error_embed)
-        raise getattr(exc, "original", exc)
-
-    async def on_command_error(self, ctx, exc):
         error_exception = getattr(exc, "original", exc)
         error_embed = discord.Embed(description = f"Error\n\n**{error_exception}**", color=0x303136)
         raise getattr(exc, "original", exc)
 
-    async def on_ready(self):
-        self.client_id = (await self.application_info()).id
-        guild = self.get_guild(rdfID)
-        registeredTime = now.strftime("%A  %d-%m-%Y  %H:%M:%S")     # the time at which the bot started
-        print(f"Bot started at:            {registeredTime}")
-        print(f"Bot logged in as:          {self.user.name}")
-        print(f"Bot running on server:     {guild}")        
-        print("Bot ready.")
+    # async def on_ready(self):
+    #     self.client_id = (await self.application_info()).id
+    #     guild = self.get_guild(rdfID)
+    #     registeredTime = now.strftime("%A  %d-%m-%Y  %H:%M:%S")     # the time at which the bot started
+    #     print(f"Bot started at:            {registeredTime}")
+    #     print(f"Bot logged in as:          {self.user.name}")
+    #     print(f"Bot running on server:     {guild}")        
+    #     print("Bot ready.")
 
     async def prefix(self, bot, msg):
         return commands.when_mentioned_or("+")(bot, msg)
--- cogs/eventSetup.py
+++ cogs/eventSetup.py
@@ -35,10 +35,10 @@ class EventSetup(commands.Cog):
 
         if dt_role in member.roles:
             self.load_data()
-            new_events_channel = self.bot.get_channel(self.id["events_channel"])
+            new_events_channel = self.bot.get_channel(self.id["new_events_channel"])
             if new_events_channel == ctx.channel:
                 await new_events_channel.purge(limit = 50)
-                await self.NewEventMsg
+                await self.NewEventMsg()
             else:
                 await ctx.channel.send("Wrong channel")
 
@@ -47,8 +47,8 @@ class EventSetup(commands.Cog):
         new_event_channel = self.bot.get_channel(self.id["new_events_channel"])
 
         new_event_embed = discord.Embed(title = "Create new events here!", color=0x303136)
-        new_event_embed.add_field(name = "\u200B", value = "**React to this mesage with the type of event you would like to create", inline = False)
-        new_event_embed.add_field(name = "\u200B", value = ":alarm_clock: Doodle(Date finder)\n:date: Meeting\n:game_die: Game Jam")
+        new_event_embed.add_field(name = "\u200B", value = "**React to this mesage with the type of event you would like to create**", inline = False)
+        new_event_embed.add_field(name = "\u200B", value = ":alarm_clock: Doodle (Date finder)\n:date: Meeting\n:game_die: Game Jam")
         new_event_msg = await new_event_channel.send(embed = new_event_embed)
         new_event_msg_id = new_event_msg.id
 
@@ -66,20 +66,179 @@ class EventSetup(commands.Cog):
             return
 
         elif payload.message_id == self.id['new_event_msg_id']:
-            guild = self.got.get_guild(payload.guild_id)
-            events_channel = self.bot.get_channel(self.id['events_channel'])
+            guild = self.bot.get_guild(payload.guild_id)
+            new_events_channel = self.bot.get_channel(self.id['new_events_channel'])
             reaction = payload.emoji.name
 
             event_type = "none"
             if reaction == '\u23F0':            # alarm clock
-                event_type = "doodle"
+                event_type = "Doodle"
             elif reaction == '\U0001F4C5':
-                event_type = "meeting"
+                event_type = "Meeting"
             elif reaction == '\U0001F3B2':
-                event_type = "jam"
-            await events_channel.purge(limit = 5)
+                event_type = "Game Jam"
+            await new_events_channel.purge(limit = 5)
 
-            date_embed = discord.Embed(title = f"You have chosen to create a **{event_type}** event", color=0x303136)
+# DESCRIPTION
+            desc_embed = discord.Embed(title = f"You have chosen to create a **{event_type}** event", color=0x303136)
+            desc_embed.add_field(name = '\u200B', value = "Please enter a description for the event.\nMust not be longer than 1020 characters.", inline = False)
+            desc_embed.add_field(name = '\u200B', value = ":octagonal_sign: = cancel")
+            desc_embed_msg = new_events_channel.send(embed = desc_embed)
+            await desc_embed.add_reaction(emoji = '\U0001F6D1')     # :octagonal_sign:
+            await asyncio.sleep(1) 
+
+            input_msg = await self.bot.wait_for('message', check=lambda message: message.author == message.author and message.channel == new_events_channel)
+            input_msg = input_msg.content
+            await new_events_channel.purge(5)
+
+            if input_msg == "cancel":
+                await self.NewEventMsg()
+                return
+
+            event_desc = ""
+            if len(input_msg) < 1020:
+                event_desc = input_msg
+            else:
+                await new_events_channel.send(input_msg)
+                desc_too_long_embed = discord.Embed(title = "Your description includes more than 1020 characters. Reduce the amount of characters in order to make an event!")
+                await new_events_channel.send(embed = desc_too_long_embed)
+                await asyncio.sleep(6)
+                await self.NewEventMsg
+                return 
+
+# DOODLE
+    # Doodle start
+            if event_type == "Doodle":
+                doodle_start_embed = discord.Embed(title = "The description has been set!", color=0x303136)
+                doodle_start_embed.add_field(name = "\u200B", value = "**What date should the doodle start?**\nWrite in this channel.\n*Ex: 2021-01-31*", inline = False)
+                doodle_start_embed.add_field(name = '\u200B', value = ":octagonal_sign: = cancel")
+                doodle_start_embed_msg = await new_events_channel.send(embed = doodle_start_embed)
+                await doodle_start_embed_msg.add_reaction(emoji = '\U0001F6D1')     # :octagonal_sign:
+                await asyncio.sleep(1)
+
+                input_msg = await self.bot.wait_for('message', check=lambda message: message.author == message.author and message.channel == new_events_channel)
+                input_msg = input_msg.content
+                await new_events_channel.purge(limit = 5)
+
+                if input_msg == "cancel":
+                    await self.NewEventMsg()
+                    return
+
+                doodle_start = ""
+                try:
+                    self.validate_date(input_msg)
+                    if self.validate_date(input_msg) == None:
+                        doodle_start = input_msg
+                except ValueError:
+                    await new_events_channel.send("You have entered a wrong date format.\nFormat must be **YYYY-MM-DD**.\nTry again!")
+                    await asyncio.sleep(5)
+                    await self.NewEventMsg()
+                    return
+
+    # Doodle end
+                doodle_end_embed = discord.Embed(title = f"Doodle will start on **{doodle_start}", color = 0x303136)
+                doodle_end_embed.add_field(name = "\u200B", value = "**When should the doodle end?**\nWrite in this channel.\n*Ex: 2021-01-31*", inline = False)
+                doodle_end_embed.add_field(name = '\u200B', value = ":octagonal_sign: = cancel")
+                doodle_end_embed_msg = await new_events_channel.send(embed = doodle_end_embed)
+                await doodle_end_embed_msg.add_reaction(emoji = '\U0001F6D1')     # :octagonal_sign:
+                await asyncio.sleep(1)
+                
+                input_msg = await self.bot.wait_for('message', check=lambda message: message.author == message.author and message.channel == new_events_channel)
+                input_msg = input_msg.content
+                await new_events_channel.purge(limit = 5)
+
+                if input_msg == "cancel":
+                    await self.NewEventMsg()
+                    return
+
+                doodle_end = ""
+                try:
+                    self.validate_date(input_msg)
+                    if self.validate_date(input_msg) == None:
+                        doodle_end = input_msg
+                except ValueError:
+                    await new_events_channel.send("You have entered a wrong date format.\nFormat must be **YYYY-MM-DD**.\nTry again!")
+                    await asyncio.sleep(5)
+                    await self.NewEventMsg()
+                    return
+
+# MEETING / GAME JAM
+    # DATE
+            if event_type == "Meeting" or event_type == "Game Jam":
+                date_embed = discord.Embed(title = "The description has been set!", color=0x303136)
+                date_embed.add_field(name = '\u200B', value = "**What date should the event take place?**\nWrite in this channel.\n*Ex: 2021-01-31*", inline = False)
+                date_embed.add_field(name = '\u200B', value = ":octagonal_sign: = cancel")
+                date_embed_msg = await new_events_channel.send(embed = date_embed)
+                await date_embed_msg.add_reaction(emoji = '\U0001F6D1')     # :octagonal_sign:
+                await asyncio.sleep(1)
+
+                input_msg = await self.bot.wait_for('message', check=lambda message: message.author == message.author and message.channel == new_events_channel)
+                input_msg = input_msg.content
+                await new_events_channel.purge(limit = 5)
+
+                if input_msg == "cancel":
+                    await self.NewEventMsg()
+                    return
+
+                event_date = ""
+                try:
+                    self.validate_date(input_msg)
+                    if self.validate_date(input_msg) == None:
+                        event_date = input_msg
+                except ValueError:
+                    await new_events_channel.send("You have entered a wrong date format.\nFormat must be **YYYY-MM-DD**.\nTry again!")
+                    await asyncio.sleep(5)
+                    await self.NewEventMsg()
+                    return
+
+    # TIME
+                time_embed = discord.Embed(title = f"The chosen date for **{event_type} event** is **{event_date}", color=0x303136)
+                time_embed.add_field(name = '\u200B', value = "**What time of day should the event take place?**\nWrite in this channel.\n*Ex: 18:00*", inline = False)
+                time_embed.add_field(name = '\u200B', value = ":octagonal_sign: = cancel")
+                time_embed_msg = await new_events_channel.send(embed = time_embed)
+                await time_embed_msg.add_reaction(emoji = '\U0001F6D1')     # :octagonal_sign:
+                await asyncio.sleep(1)
+
+                input_msg = await self.bot.wait_for('message', check=lambda message: message.author == message.author and message.channel == new_events_channel)
+                input_msg = input_msg.content
+                await new_events_channel.purge(5)
+
+                if input_msg == "cancel":
+                    await self.NewEventMsg()
+                    return
+                
+                event_time = ""
+                try:
+                    self.validate_time(input_msg)
+                    if self.validate_time(input_msg) == None:
+                        event_time = input_msg
+                except ValueError:
+                    await new_events_channel.send("You have entered a wrong time date format.\nFormat must be **HH:MM**.\nTry again!")
+                    await asyncio.sleep(5)
+                    await self.NewEventMsg()
+                    return
+
+    # DATE AND TIME CHECK
+                event_date_time = f"{event_date} {event_time}"
+                if event_date_time in self.event:
+                    if event_date == self.event[event_date_time]['date'] and event_time == self.event[event_date_time]['time']:
+                        await new_events_channel.send("An event already exists on this date and time.\nTry again with another date or time!")
+                        await asyncio.sleep(5)
+                        await new_events_channel.purge(5)
+                        await self.NewEventMsg()
+                        return 
+
+                complete_embed = discord.Embed(title = f"a new event type **{event_type}** has been created!")
+                complete_embed.add_field(name = "**Date**", value = event_date, inline = False)
+                complete_embed.add_field(name = "**Time**", value = event_time, inline = False)
+                complete_embed.add_field(name = "Description", value = event_desc, inline = False)
+                await new_events_channel.send(embed = complete_embed)
+                await asyncio.sleep(6)
+                await new_events_channel.purge(5)
+                
+
+def setup(bot):
+    bot.add_cog(EventSetup(bot))
             
         
 
